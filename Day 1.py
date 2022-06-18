3267785102facdcc5d272b21686dc9def9e4da00
Jibinlal <76659024+Jibinlal@users.noreply.github.com>
2022-06-18 17:16:57 -0400

    Day 1 -Final

--- Day 1.py
+++ Day 1.py
@@ -28,5 +28,8 @@ print("b: " + b)
 
 
 
-
-
+# Day 1 project - Band Name generator
+print ("Welcome to the Band Name Generator")
+city = input("What's the name of the city you grew up in?\n")
+pet = input("What's your pet's name?\n")
+print ("Your band name could be "+city + " "+pet)
