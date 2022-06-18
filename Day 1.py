af9cf852762108f01c31816a7f36c7f055ca0acd
Jibinlal <76659024+Jibinlal@users.noreply.github.com>
2022-06-18 15:53:29 -0400

    Day 1 complete

--- Day 1.py
+++ Day 1.py
@@ -1,3 +1,32 @@
-msg = "Hello"
-print(msg)
-print(msg.upper())
+#Day 1 
+
+# - Printing start
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+#Fix the code below 👇
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+# Write a program that prints the number of characters in a user's name. 
+print(len(input("What is your name?")))
+
+# Write a program that switches the values stored in the variables a and b.
+a = input("a: ")
+b = input("b: ")
+
+c=a 
+a=b
+b=c
+
+print("a: " + a)
+print("b: " + b)
+
+
+
+
+
